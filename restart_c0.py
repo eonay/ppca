cfca21b94b29c8d143be5054e05bf3d0275e3ab5
Eonay <eonay.web@gmail.com>
2021-03-25 15:32:47 -0300

    revisado

--- restart_c0.py
+++ restart_c0.py
@@ -1,8 +1,7 @@
-#!/bin/bash
+#!/usr/bin/python3
 #
-
+#
 import paramiko
-
 ssh = paramiko.SSHClient()
 ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 ssh.connect(hostname='192.168.88.111',username='root',password='root')	
