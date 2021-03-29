17c7608a785a01e873bffdddbc8cf8924ae6deb5
Eonay <eonay.web@gmail.com>
2021-03-29 15:40:28 -0300

    ajuste permissão arquivo retorno

--- multi_controller/statistic/run_tests.py
+++ multi_controller/statistic/run_tests.py
@@ -1,44 +1,34 @@
 #
-import paramiko
 import os
+
+import paramiko
 from scp import SCPClient
 
-ssh = paramiko.SSHClient()
-ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-ssh.connect(hostname='192.168.219.111',username='root',password='root')	
-ssh.exec_command('nohup vmstat -h 1 150 >> c0-cpu_memory-v1.out &')
-###ssh.close()
-os.system('sh c0-run_cbench.sh')
+controller1='172.16.187.129'
+controller2='172.16.187.130'
+
+print('***Controller[1]...')
+ssh1 = paramiko.SSHClient()
+ssh1.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+ssh1.connect(hostname=controller1,username='root',password='root')	
+ssh1.exec_command('nohup vmstat 1 220 >> c1-cpu_memory-v1.out &')
+os.system('sh run_cbench.sh c1')
+with SCPClient(ssh1.get_transport()) as scp:
+    scp.get('/root/c1-cpu_memory-v1.out')
+ssh1.close()
+print('[OK]')
 
 
-with SCPClient(ssh.get_transport()) as scp:
-    scp.get('/root/c0-cpu_memory-v1.out')
-
-ssh.close()
-#sftp.close()
-'''
-os.system('scp root@192.168.219.111:/root/cpu_memory.out .')
-
-with SCPClient(ssh.get_transport()) as scp:
-    scp.put('test.txt', 'test2.txt')
-    scp.get('test2.txt')
-'''
-
-
-
+print('***Controller[2]...')
 ssh2 = paramiko.SSHClient()
 ssh2.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-ssh2.connect(hostname='192.168.219.222',username='root',password='root')	
-ssh2.exec_command('nohup vmstat -h 1 150 >> c1-cpu_memory-v1.out &')
-###ssh.close()
-os.system('sh c1-run_cbench.sh')
-
-
+ssh2.connect(hostname=controller2,username='root',password='root')	
+ssh2.exec_command('nohup vmstat 1 220 >> c2-cpu_memory-v1.out &')
+os.system('sh run_cbench.sh c2')
 with SCPClient(ssh2.get_transport()) as scp:
-    scp.get('/root/c1-cpu_memory-v1.out')
-
+    scp.get('/root/c2-cpu_memory-v1.out')
 ssh2.close()
-
+print('[OK]')
 
 os.system('sudo chmod 777 *')
 
